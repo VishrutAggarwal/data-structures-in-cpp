3f0a4cbfe39e9a46e2200f0529df6f205f4e4d77
Vishrut Aggarwal <vishrut.agg@gmail.com>
2022-11-24 06:24:11 +0530

    add: add binary traversal techniques

---
+++ Binary Trees/preorder-traversal.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> sol;
+
+        helper(root, sol);
+        return sol;
+    }
+
+    void helper(TreeNode* ptr, vector<int>& sol) {
+        if(ptr) {
+            sol.push_back(ptr->val);
+            helper(ptr->left, sol);
+            helper(ptr->right, sol);
+        }
+    }
+};
